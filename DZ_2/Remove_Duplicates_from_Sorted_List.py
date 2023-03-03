5be8726cf918791009ccd9aca7342c79a2256e6b
LeoKlen <88614809+LeoKlen@users.noreply.github.com>
2023-03-03 07:29:34 +0300

    Update Remove_Duplicates_from_Sorted_List.py

--- DZ_2/Remove_Duplicates_from_Sorted_List.py
+++ DZ_2/Remove_Duplicates_from_Sorted_List.py
@@ -1,12 +1,12 @@
 #https://leetcode.com/problems/remove-duplicates-from-sorted-list/
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:  
-        
+        if not head:
+            return head
         head_now = head
         head_next = head.next
         ans=[]
-        if not head:
-            return head
+        
         while head_next != None:
             if head_now.val == head_next.val:
 
