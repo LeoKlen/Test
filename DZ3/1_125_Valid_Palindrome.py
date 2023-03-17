6d9d41b1b0ddd90a0d5ea592a22e97975a0b3c9b
LeoKlen <88614809+LeoKlen@users.noreply.github.com>
2023-03-17 11:05:00 +0300

    Update 1_125_Valid_Palindrome.py

--- DZ3/1_125_Valid_Palindrome.py
+++ DZ3/1_125_Valid_Palindrome.py
@@ -1,22 +1,22 @@
 #https://leetcode.com/problems/valid-palindrome/description/
 class Solution:
     def isPalindrome(self, string: str) -> bool:
-        
         l = 0
-        r = len(string)-1
+        r = len(string) - 1
 
-        while l<r:
+        while l < r:
+            print(string[l])
+            print(string[r])
+            while not(string[l].isalpha() or string[l].isdigit())and l < r :
+                l += 1
 
-            while string[l] == ' ' and l < r:
-                l+=1
+            while not(string[r].isalpha() or string[r].isdigit()) and l < r :
+                r -= 1
 
-            while string[r] == ' '  and l < r:
-                r-=1
-            
-            if string[l] != string[r]:
+            if string[l].lower() != string[r].lower():
                 return False
 
-            l+=1
-            r-=1
+            l += 1
+            r -= 1
 
         return True
