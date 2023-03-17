a5a1e4653a481045efcadcb033b135fd8efc09f6
LeoKlen <88614809+LeoKlen@users.noreply.github.com>
2023-03-17 11:39:17 +0300

    Update 8_Convert_string_to_camel_case.py

--- DZ3/8_Convert_string_to_camel_case.py
+++ DZ3/8_Convert_string_to_camel_case.py
@@ -1,13 +1,25 @@
 #https://www.codewars.com/kata/517abf86da9663f1d2000003/train/python
 def to_camel_case(text):
+    l=0
+    r=1
     if not text:
-        return ''
+        return text
+    s=text[0]
+    #print(text)
     
-    ans = ''
-    for i in ['-','_']:
-        text = text.replace(i,' ')
-    text = text.split(' ')
-    ans+=text[0]
-    for i in range(1,len(text)):
-        ans+=text[i].capitalize()
-    return ans
+
+    while r!=len(text):
+        if text[r] == '_' or text[r] == '-':
+            r+=1
+
+            s+=text[r].capitalize()
+
+            continue
+        if s[len(s)-1]==text[r].upper():
+            r+=1
+            continue
+        else:
+            s+=text[r]
+            r+=1
+    return s
+        
