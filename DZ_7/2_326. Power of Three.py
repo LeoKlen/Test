895967a65934db419d5874168bf3ae5cb3ef4a3c
LeoKlen <88614809+LeoKlen@users.noreply.github.com>
2023-04-21 11:30:36 +0300

    Create 2_326. Power of Three.py

---
+++ DZ_7/2_326. Power of Three.py
@@ -0,0 +1,8 @@
+#https://leetcode.com/problems/power-of-three/description/
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+        return self.isPowerOfThree(n/3)
